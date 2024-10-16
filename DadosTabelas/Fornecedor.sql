bdd6d68d4dad006eebf3dd6325f5f0da93e41fb6
HeloisaJ <heloisananda21@gmail.com>
2024-10-16 09:59:55 -0300

    Fiz as inserções nas tabelas da parte1

---
+++ DadosTabelas/Fornecedor.sql
@@ -0,0 +1,11 @@
+INSERT INTO fornecedor (nome, estado_origem) VALUES
+('Granja do Sol', 'Pernambuco'),
+('Laticínios Alvez', 'Minas Gerais'),
+('Distribuidora SP', 'Sao Paulo'),
+('Fazenda Cooperativa', 'Paraiba'),
+('Distribuidora Atacadão', 'Pernambuco'),
+('Fábrica de Doces', 'Minas Gerais'),
+('Fábrica de Chocolate', 'Rio Grande do Sul'),
+('Fábrica da Perdigão', 'Santa Catarina'),
+('Salina Cristal', 'Piaui'),
+('Fazenda Azul', 'Rio Grande do Norte');
