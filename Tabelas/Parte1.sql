271c6e73b0dd069d1940045431e328dc17c958ed
HeloisaJ <heloisananda21@gmail.com>
2024-10-15 22:46:11 -0300

    Melhoria na estrutura das tabelas

--- Tabelas/Parte1.sql
+++ Tabelas/Parte1.sql
@@ -1,21 +1,21 @@
 CREATE TABLE fornecedor(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(40),
-    estado_origem VARCHAR(19)
+    nome VARCHAR(40) NOT NULL,
+    estado_origem VARCHAR(19) NOT NULL
 );
 
 CREATE TABLE ingredientes(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(30),
-    data_fabricacao DATE,
-    data_validade DATE,
-    quantidade SMALLINT,
+    nome VARCHAR(30) NOT NULL,
+    data_fabricacao DATE NOT NULL,
+    data_validade DATE NOT NULL,
+    quantidade SMALLINT NOT NULL,
     observacao VARCHAR(100)
 );
 
 CREATE TABLE usos(
-	id_prato INT,
-    id_ingrediente INT,
+	id_prato INT NOT NULL,
+    id_ingrediente INT NOT NULL,
 	FOREIGN KEY (id_prato) REFERENCES prato(id),
     FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id)
 );
