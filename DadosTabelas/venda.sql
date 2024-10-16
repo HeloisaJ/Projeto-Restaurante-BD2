d6856b91cd8b100fa3953b15c9d093593892cac8
princevegeta <princevegeta@kingkong.com>
2024-10-15 23:35:31 -0300

    feat_criação_tabelas_venda_prato_cliente

---
+++ DadosTabelas/venda.sql
@@ -0,0 +1,12 @@
+-- Inserindo dados na tabela de vendas
+INSERT INTO venda (id_cliente, id_prato, quantidade, dia, hora, valor) VALUES
+(1, 1, 2, '2024-10-15', '19:30:00', 60.00),  -- Goku, 2 Margueritas
+(2, 3, 1, '2024-10-14', '20:00:00', 28.00),  -- Vegeta, 1 Muçarela
+(3, 4, 3, '2024-10-13', '18:00:00', 96.00),  -- Bulma, 3 Peperonis
+(4, 2, 1, '2024-10-12', '21:00:00', 25.00),  -- Gohan, 1 Chocolate
+(5, 5, 2, '2024-10-11', '19:00:00', 56.00),  -- Piccolo, 2 Calabresas
+(6, 6, 1, '2024-10-10', '20:30:00', 34.00),  -- Krillin, 1 Frango com Catupiry
+(7, 7, 4, '2024-10-09', '19:15:00', 140.00), -- Frieza, 4 Bacons
+(8, 8, 1, '2024-10-08', '18:45:00', 35.00),  -- Android 18, 1 Abacaxi
+(9, 9, 2, '2024-10-07', '20:00:00', 66.00),  -- Trunks, 2 Portuguesas
+(10, 10, 1, '2024-10-06', '19:30:00', 36.00); -- Chi-Chi, 1 Mista
