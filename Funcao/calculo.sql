76caeaeb2e24dfe5c8f60869b9014f58e1c1e83e
HeloisaJ <heloisananda21@gmail.com>
2024-10-21 19:05:57 -0300

    Criada a função de cálculo de pontos

---
+++ Funcao/calculo.sql
@@ -0,0 +1,10 @@
+-- Recebe um valor representando a compra e retorna a quantidade de pontos referente à compra
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `calculo_pontos`(valor DECIMAL(5, 2)) RETURNS int(11)
+BEGIN
+	DECLARE pontos INT;
+    
+    SET pontos := FLOOR(valor/10);
+    
+RETURN pontos;
+END
