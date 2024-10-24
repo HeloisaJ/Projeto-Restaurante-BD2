a44159992e6f8b5eb638e245b6608a0ae9c9b40d
PedroPepeu <pedrosteamgtav@gmail.com>
2024-10-24 10:40:14 -0300

    Trigger done

--- Triggers/Trigger1.sql
+++ Triggers/Trigger1.sql
@@ -1,12 +1,25 @@
 -- [Venda] Sempre que ocorrer uma venda reduzir em 1 a quantidade do produto (pepeu)
+DROP TRIGGER IF EXISTS sales_update;
+
 DELIMITER //
 
 CREATE TRIGGER sales_update
-AFTER UPDATE
-ON table_name -- ver em prato quais os ingredientes necessarios e as quantidades necessarias e reduzir todos pela quanditade utilizada
+AFTER INSERT
+ON venda -- apos a venda, diminuir a quantidade de ingredientes
 FOR EACH ROW
 BEGIN
     -- TRIGGER body
+	UPDATE ingredientes
+	SET quantidade = quantidade - NEW.quantidade
+	WHERE id IN
+	(
+		SELECT 
+			a.id_ingrediente
+		FROM 
+			usos a
+		WHERE 
+			a.id_prato = NEW.id_prato 
+	);
 END//
 
 DELIMITER ;
