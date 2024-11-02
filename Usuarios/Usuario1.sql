97d93317eb18f5d53575beeb984ba23b2cfd4ee1
ghts2110 <gabrieltorres2110@gmail.com>
2024-11-02 10:57:30 -0300

    usuarios e permissoes

---
+++ Usuarios/Usuario1.sql
@@ -0,0 +1,8 @@
+CREATE USER 'Administrador'@'localhost' IDENTIFY BY '123';
+CREATE USER 'Gerente'@'localhost' IDENTIFY BY '456';
+CREATE USER 'Funcionario'@'localhost' IDENTIFY BY '789';
+
+GRANT ALL PRIVILEGES ON restaurante.* TO 'Administrador'@'localhost';
+GRANT SELECT, UPDATE, DELETE ON restaurante.* TO 'Gerente'@'localhost';
+GRANT INSERT, SELECT ON restaurante.venda TO 'Funcionario'@'localhost';
+
