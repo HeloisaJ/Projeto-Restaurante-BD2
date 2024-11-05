1bdf1111efbf5c3a28e8fc79133541004ba8cd48
ghts2110 <gabrieltorres2110@gmail.com>
2024-11-05 07:51:35 -0300

    trigger ajuste test

--- Triggers/Trigger4.sql
+++ Triggers/Trigger4.sql
@@ -11,8 +11,8 @@ BEGIN
     
     -- Verifica se o prato está disponível
     IF (SELECT disponibilidade FROM prato WHERE id = NEW.id_prato) = FALSE THEN
-        SET mensagem_erro = CONCAT('Erro: O prato "', (SELECT nome FROM prato WHERE id = NEW.id_prato), '" está indisponível para venda.');
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = mensagem_erro;
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Erro: O prato está indisponível para venda.';
     END IF;
 END //
 
