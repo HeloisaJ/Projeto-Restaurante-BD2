d6cfb149bfd25becd0c124145154d7572bf4b2e4
ghts2110 <gabrieltorres2110@gmail.com>
2024-11-05 10:49:00 -0300

    valtar commit

--- Triggers/Trigger4.sql
+++ Triggers/Trigger4.sql
@@ -3,21 +3,25 @@ DROP TRIGGER IF EXISTS verifica_disponibilidade_prato;
 
 DELIMITER //
 
+DELIMITER //
+
 CREATE TRIGGER verifica_disponibilidade_prato
 BEFORE INSERT ON venda
 FOR EACH ROW
 BEGIN
+    DECLARE disponibilidade_prato BOOLEAN;
+
+    -- Obtém a disponibilidade do prato
+    SELECT disponibilidade INTO disponibilidade_prato
+    FROM prato 
+    WHERE id = NEW.id_prato
+    LIMIT 1;
+
     -- Verifica se o prato está disponível
-    IF (SELECT disponibilidade FROM prato WHERE id = NEW.id_prato) = FALSE THEN
+    IF disponibilidade_prato = FALSE THEN
         SIGNAL SQLSTATE '45000' 
         SET MESSAGE_TEXT = 'Erro: O prato está indisponível para venda.';
     END IF;
 END //
 
 DELIMITER ;
-
-
-
-
-
-
