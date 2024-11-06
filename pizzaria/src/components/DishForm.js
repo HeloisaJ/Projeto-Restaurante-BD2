e88ba82ce6f3f7b0ffa4607784a5422d031cce83
PedroPepeu <pedrosteamgtav@gmail.com>
2024-11-06 19:00:59 -0300

    adding example

--- pizzaria/src/components/DishForm.js
+++ pizzaria/src/components/DishForm.js
@@ -1,40 +1,49 @@
-import React, { useState } from 'react';
-import styles from '../styles/DishForm.module.css';
+import React, { useState } from "react";
+import styles from "../styles/DishForm.module.css";
 
 function DishForm() {
-    const [dishName, setDishName] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    
+    const [dishName, setDishName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const newDish = { dishName, description, price };
+        const newDish = {
+            nome: dishName,
+            descricao: description,
+            valor: price,
+            disponibilidade: 1,
+        };
 
         try {
-            const response = await fetch('http://localhost:3001/api/prato', {
-                method: 'POST',
+            const response = await fetch("http://localhost:3001/api/prato", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify(newDish),
             });
 
             if (!response.ok) {
-                throw new Error('Failed to submit the dish data, try again later');
+                throw new Error(
+                    "Failed to submit the dish data, try again later",
+                );
             }
 
             const postResult = await response.json();
-            console.log('Response from server: ', postResult);
-            alert('Dish added successfully!');
-            
+            console.log("Response from server: ", postResult);
+            alert("Dish added successfully!");
+
             // Reset the form after successful submission
-            setDishName('');
-            setDescription('');
-            setPrice('');
-        } 
-        catch (error) {
-            console.error('An error occurred while submitting the dish data:', error);
-            alert('An error occurred while adding the dish');
+            setDishName("");
+            setDescription("");
+            setPrice("");
+        } catch (error) {
+            console.error(
+                "An error occurred while submitting the dish data:",
+                error,
+            );
+            alert("An error occurred while adding the dish");
         }
     };
 
